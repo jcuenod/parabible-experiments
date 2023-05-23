8e0997cd3a0533405dd27a75c4f042b186742406
James Cuénod <4253884+jcuenod@users.noreply.github.com>
2023-05-22 19:41:24 -0500

    Friendly names in word info + styling

--- doric-parabible/src/components/doric-widgets/WordInfo.vue
+++ doric-parabible/src/components/doric-widgets/WordInfo.vue
@@ -1,5 +1,22 @@
 <script setup lang="ts">
 import { watch, ref } from "vue"
+import { features, values } from "@/assets/features.json"
+import { isInteger } from "lodash-es"
+
+type FeatureKey = { key: string, value: string, enum: boolean }
+type FeatureValue = { feature: string, key: string, value: string }
+const friendlyKey = (key: string) => {
+    const feature = features.find((f: FeatureKey) => f?.key === key)
+    return feature ? feature.value : key
+}
+const friendlyValue = (key: string, value: string) => {
+    const feature = features.find((f: FeatureKey) => f?.key === key)
+    if (feature?.enum) {
+        return values.find((v: FeatureValue) => v.feature === key && v.key === value)?.value || value
+    }
+    return value
+}
+
 const props = defineProps({
     useDoricInput: {
         type: Function,
@@ -23,8 +40,9 @@ const state = ref(STATE.INITIAL)
 
 watch(() => activeWord.value, () => {
     const { wid, moduleId } = JSON.parse(activeWord.value)
-    if (wid && moduleId) {
-        console.log(`wid: ${wid}, moduleId: ${moduleId}`)
+    if (!isInteger(wid) || !isInteger(moduleId)) {
+        console.error("WordInfo: activeWord is not a valid word")
+        return
     }
     state.value = STATE.LOADING
     fetch(`https://dev.parabible.com/api/v2/word?wid=${wid}&moduleId=${moduleId}`)
@@ -49,10 +67,24 @@ watch(() => activeWord.value, () => {
         <div v-else-if="state === STATE.LOADED">
             <table>
                 <tr v-for="word in wordFeatures" :key="word.wid">
-                    <td>{{ word.key }}</td>
-                    <td>{{ word.value }}</td>
+                    <td>{{ friendlyKey(word.key) }}</td>
+                    <td>{{ friendlyValue(word.key, word.value) }}</td>
                 </tr>
             </table>
         </div>
     </div>
 </template>
+
+<style>
+table {
+    width: 100%;
+}
+
+tr:hover {
+    background-color: #eee;
+}
+
+td {
+    padding: 0.2rem 0.5rem;
+}
+</style>
