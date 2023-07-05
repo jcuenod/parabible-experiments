3f92f9e2f68ff3bc39411224ccc2542f11ca390c
James Cuénod <4253884+jcuenod@users.noreply.github.com>
2023-07-05 11:08:07 -0500

    Update doric + code cosmetics

--- doric-parabible/src/App.vue
+++ doric-parabible/src/App.vue
@@ -2,11 +2,16 @@
 import { ref } from 'vue'
 import { DoricFramework } from 'doric-framework'
 import "doric-framework/dist/style.css"
-import widgets from '@/components/doric-widgets/Widgets'
-const workspace = ref([[
-  { type: "passage-reference" }, { type: "active-modules" }],
-[{ type: "chapter-display" }],
-[{ type: "word-info" }]
+import widgets from '@/components/doric-widgets/'
+
+const workspace = ref([
+  [
+    { type: "passage-reference" }, { type: "active-modules" }
+  ], [
+    { type: "chapter-display" }
+  ], [
+    { type: "word-info" }
+  ]
 ])
 
 </script>
